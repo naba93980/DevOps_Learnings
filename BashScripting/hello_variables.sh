88b1a7fb316694ba18cd10619a9eb82bc26637b3
NabajyotiModak <naba93981@gmail.com>
2023-01-22 21:14:18 +0530

    shebang, comment,echo, variables

---
+++ BashScripting/hello_variables.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# this is a comment
+echo "HelloWorld"
+
+
+# system variables
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo 'current working directory' $PWD
+
+
+# user-defined variables
+name='NABAJYOTI MODAK'
+echo The name is $name
