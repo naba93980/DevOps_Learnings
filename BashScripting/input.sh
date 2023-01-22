1603c1d21bae4de4d5e72302abb2e017fdae1b84
NabajyotiModak <naba93981@gmail.com>
2023-01-22 22:24:48 +0530

    input with different flags

---
+++ BashScripting/input.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "Enter name: "
+read  name1 name2            # takes anything upto first space as input for name1           #backlash acts as an escape character
+echo "name 1 - $name1, name 2 - $name2 "
+
+
+echo "Enter roll: "
+read -r name                        # backlash does not acts as an escape character
+echo "$name"
+
+read -p "please type ur name " name  # -p promt and input in same line
+echo "ur name is $name"
+
+read -s -p "please type ur password " password  # -s -input is hidden
+echo "ur password is $password"
+
+echo "Enter names : "
+read -a names                                                  # taking input as array
+echo "Names : ${names[0]}, ${names[1]}"
+
+
+
+
+
+
+# https://linuxhint.com/use-read-command-bash/#:~:text=In%20Bash%20scripting%2C%20the%20%E2%80%9Cread,inputted%20information%20from%20the%20user.
