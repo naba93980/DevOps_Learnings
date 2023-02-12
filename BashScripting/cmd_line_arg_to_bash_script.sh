4aa14cb690c4d917942ebac1f913a7d749f58207
NabajyotiModak <naba93981@gmail.com>
2023-02-12 21:43:45 +0530

    updated

--- BashScripting/cmd_line_arg_to_bash_script.sh
+++ BashScripting/cmd_line_arg_to_bash_script.sh
@@ -2,10 +2,17 @@
 
 echo "first arg: $0,  second: $1, third: $2, fourth: $3"
 
-args=("$@");
-
-echo "${args[@]}"
-
+# prints all the args
 echo "$@"
 
+# prints numbe of all args
 echo "$#"
+
+# creates array of args
+argumentArray=("$@");
+
+# prints all the args
+echo "${argumentArray[@]}"
+
+# prints args at index 1
+echo "${argumentArray[1]}"
